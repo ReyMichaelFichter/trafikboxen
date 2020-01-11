ce73143b9c37477912348cf2447ab8c7cae807ce
Michael Fichter <michael.fichter@klarna.com>
2020-01-11 15:49:13 +0100

    Write simple method to fetch station ID

--- trafikboxen/__main__.py
+++ trafikboxen/__main__.py
@@ -1,9 +1,27 @@
 """Entrypoint for the package"""
-from time import sleep
+import os
 from typing import Any
 
+import requests
 
-def fetch_departures(station: Any) -> Any:
+LUMA_STATION_ID = "1552"
+
+
+def fetch_sl_station_id(search_string: str) -> str:
+    """
+    Fetch an SL station ID based on a search string
+    :param search_string: Search string
+    :return: station ID
+    """
+    params = {
+        "key": os.environ["PLATSUPPSLAG_API_KEY"],
+        "SearchString": search_string,
+    }
+    res = requests.get("https://api.sl.se/api2/typeahead.json", params)
+    return res.json()["ResponseData"][0]["SiteId"]
+
+
+def fetch_departures(station_id: str = "") -> Any:
     """
     Fetch departure information for a station
     :return: Departure information
@@ -29,6 +47,4 @@ def main():
 
 
 if __name__ == "__main__":
-    while True:
-        main()
-        sleep(15)
+    print(fetch_sl_station_id("luma"))
